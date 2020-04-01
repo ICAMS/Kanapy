6913910d6f5ebd6d5e9c6b26f5fe789b600aedb3
Mahesh R.G Prasad <mahesh.prasad@rub.de>
2020-04-01 14:22:38 +0200

    Updated ROOT & MAIN directory paths in utils.py

--- src/kanapy/util.py
+++ src/kanapy/util.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))       # Kanapy's root
-MAIN_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),"../.."))
+#ROOT_DIR = os.path.dirname(os.path.abspath(__file__))       # Kanapy's root
+#MAIN_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),"../.."))
+
+ROOT_DIR = os.path.abspath(os.curdir)
+MAIN_DIR = os.path.abspath(os.curdir + "/../..")
