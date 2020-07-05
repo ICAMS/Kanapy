a9b5c8a77956c341dd0556e5bfd82cf3fa082039
Mahesh R.G Prasad <mahesh.prasad@rub.de>
2020-07-05 09:09:18 +0200

    Corrected mistake in grain volume output code

--- src/kanapy/input_output.py
+++ src/kanapy/input_output.py
@@ -1485,7 +1485,7 @@ def extract_volume_sharedGBarea():
     # Sort the grain volumes in ascending order of grain IDs
     gv_sorted_keys = sorted(grain_vol, key=grain_vol.get)
     gv_sorted_values = [[grain_vol[gk]] for gk in gv_sorted_keys]            
-    gv_sorted_values = [[gk,gv] for gk,gv in grain_vol.items()]
+#    gv_sorted_values = [[gk,gv] for gk,gv in grain_vol.items()]
     
     print("Writing grain volumes info. to file ('grainVolumes.csv')\n", end="")
         
