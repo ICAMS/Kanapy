bb9ec9c63299d18421bafd94f9499dcb4e46a495
Alexander Hartmaier <alexander@macbookpro2.icams.ruhr-uni-bochum.de>
2021-06-14 12:19:17 +0200

    GNU AGPL v3 license mentioned

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
+# This software is distributed under the  GNU AFFERO GENERAL PUBLIC LICENSE, Version 3, 19 November 2007
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+# THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 import re
 import sys
@@ -78,7 +90,7 @@ kwargs = dict(
     author='Mahesh R.G Prasad',
     author_email='mahesh.prasad@rub.de',
     classifiers=[        
-        'License :: OSI Approved :: MIT License',
+        'License :: GNU AFFERO GENERAL PUBLIC LICENSE v3',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
     ],     
@@ -86,8 +98,8 @@ kwargs = dict(
     #long_description=readme + '\n\n' + history,
     packages=find_packages('src'),
     package_dir={'':'src'},
-    license="MIT license",    
-    url='https://github.com/mrgprasad/kanapy',
+    license="GNU AGPL v3 license",    
+    url='https://github.com/ICAMS/Kanapy.git',
     #include_package_data=True,    
     ext_modules=[CMakeExtension('kanapy/base')],
     cmdclass=dict(build_ext=CMakeBuild),
