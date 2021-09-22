0bdb8957136624066a612b98d96095d3ba40db4f
Alexander Hartmaier <alexander.hartmaier@rub.de>
2021-09-22 13:58:46 +0200

    updated version number to 3.1

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ from subprocess import CalledProcessError
     
 kwargs = dict(
     name='kanapy',
-    version='2.1.1',
+    version='3.1.1',
     author='Mahesh R.G. Prasad, Abhishek Biswas, Napat Vajragupta, Alexander Hartmaier',
     author_email='alexander.hartmaier@rub.de',
     classifiers=[        
--- src/kanapy/__init__.py
+++ src/kanapy/__init__.py
@@ -2,6 +2,8 @@
 
 """Top-level package for kanapy."""
 
-__author__ = """Mahesh R.G Prasad"""
-__email__ = 'mahesh.prasad@rub.de'
-__version__ = '2.1.1'
+from kanapy.api import Microstructure
+
+__author__ = """Mahesh R.G Prasad, Abhishek Biswas, Alexander Hartmaier"""
+__email__ = 'alexander.hartmaier@rub.de'
+__version__ = '3.1.1'
