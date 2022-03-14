b546d22c6531beaab709f4ddcfc6322642927344
Jan Schmidt <jan.schmidt-p2d@rub.de>
2022-03-14 12:21:07 +0100

    Added Copper and Random texture

--- examples/polygons/example_GBtexture.py
+++ examples/polygons/example_GBtexture.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Analyse grain geometries of polycrystals, extract statistical information 
-form 2D slices, comaparable to experiment, fit ellipsoids to 3D grains 
+Analyse grain geometries of polycrystals, extract statistical information
+form 2D slices, comaparable to experiment, fit ellipsoids to 3D grains
 (polyhedra) and exctract statistical information
 
 @author: alexander Hartmaier, ICAMS / Ruhr-University Bochum, Germany
@@ -13,17 +13,19 @@ January 2022
 import numpy as np
 import matplotlib.pyplot as plt
 import kanapy as knpy
+import matlab.engine
 
+texture = 'random' # Implemented textures are goss, copper, random
 matname = 'Simulanium fcc'
 matnumber = 4  # UMAT number for fcc Iron
 Nv = 60
 size = 70
 periodic = True
-ms_elong = {'Grain type': 'Elongated', 
+ms_elong = {'Grain type': 'Elongated',
           'Equivalent diameter': {'std': 1.0, 'mean': 20.0, 'offs': 12.0, 'cutoff_min': 18.0, 'cutoff_max': 25.0},
-          'Aspect ratio': {'std':1.5, 'mean': 1.8, 'offs': 1.0, 'cutoff_min': 1.0, 'cutoff_max': 3.0}, 
-          'Tilt angle': {'std': 15., 'mean': 60., "cutoff_min": 0.0, "cutoff_max": 180.0}, 
-          'RVE': {'sideX': size, 'sideY':size, 'sideZ': size, 'Nx': Nv, 'Ny': Nv, 'Nz': Nv}, 
+          'Aspect ratio': {'std':1.5, 'mean': 1.8, 'offs': 1.0, 'cutoff_min': 1.0, 'cutoff_max': 3.0},
+          'Tilt angle': {'std': 15., 'mean': 60., "cutoff_min": 0.0, "cutoff_max": 180.0},
+          'RVE': {'sideX': size, 'sideY':size, 'sideZ': size, 'Nx': Nv, 'Ny': Nv, 'Nz': Nv},
           'Simulation': {'periodicity': str(periodic), 'output_units': 'um'},
           'Phase': {'Name': 'Simulanium fcc', 'Number': 0, 'Volume fraction': 1.0}}
 
@@ -57,10 +59,10 @@ while ndisp < 10 and igr <= ms.Ngr:
         # plot simplices of grain boundary
         grain = ms.RVE_data['Grains'][igr]
         coord = grain['Points']/ms.RVE_data['Voxel_resolutionX']
-        ax.scatter(coord[:, 0], coord[:, 1], 
+        ax.scatter(coord[:, 0], coord[:, 1],
                    coord[:, 2], marker='o', s=30, c='b')
         ax.plot_trisurf(coord[:, 0], coord[:, 1], coord[:, 2],
-                        triangles=grain['Simplices'], color=[1, 0, 0, 0.3], 
+                        triangles=grain['Simplices'], color=[1, 0, 0, 0.3],
                         edgecolor=[0.5,0.5,0.5,0.3], linewidth=1)
         ax.set(xlabel='x', ylabel='y', zlabel='z')
         ax.set_title('Grain #{}'.format(igr))
@@ -69,16 +71,17 @@ while ndisp < 10 and igr <= ms.Ngr:
         ndisp += 1
     igr += 1
 
-# compare microstructure on three surfaces 
+# compare microstructure on three surfaces
 # for voxelated and polygonalized grains
 ms.plot_slice(cut='yz', pos='top', data='poly')
 ms.plot_slice(cut='xz', pos='top', data='poly')
 ms.plot_slice(cut='xy', pos='top', data='poly')
 
+
 # Test cases for misorientation distribution (MDF)
-'''adapted from 
-Miodownik, M., et al. "On boundary misorientation distribution functions 
-and how to incorporate them into three-dimensional models of microstructural 
+'''adapted from
+Miodownik, M., et al. "On boundary misorientation distribution functions
+and how to incorporate them into three-dimensional models of microstructural
 evolution." Acta materialia 47.9 (1999): 2661-2668.
 https://doi.org/10.1016/S1359-6454(99)00137-8
 '''
@@ -89,23 +92,46 @@ if knpy.MTEX_AVAIL:
                22.1246762077, 13.9000439533],
       "Low":  [4.5317, 18.6383, 25, 20.755, 12.5984, 7.2646, 4.2648, 3.0372, 2.5,
                1, 0.31, 0.1],
-      "Random": [0.1, 0.67, 1.9, 3.65, 5.8, 8.8, 11.5, 15.5, 20, 16.7, 11.18, 4.2] 
+      "Random": [0.1, 0.67, 1.9, 3.65, 5.8, 8.8, 11.5, 15.5, 20, 16.7, 11.18, 4.2]
     }
     mdf_bins = np.linspace(62.8/12,62.8,12)
-    
+
     # generate grain orientations and write ang file
-    ang = [0, 45, 0]    # Euler angle for Goss texture
-    omega = 7.5         # kernel half-width
-    ori_rve = knpy.createOriset(ms.Ngr, ang, omega, hist=mdf_freq['Low'],
-                                shared_area=ms.shared_area)
+    '''
+    Different textures can be choosen and assinged to the RVE geometry that has
+    been defined above.
+    Texture is defined by the orientation of the ideal component in Euler space
+    ang and a kernel halfwidth omega. Kernel used here is deLaValleePoussin.
+    The function createOriset will first create an artificial EBSD by sampling
+    a large number of discrete orientations from the ODF defined by ang and
+    omega. Then a reduction method is applied to reconstruct this initial ODF
+    with a smaller number of discrete orientations Ngr. The reduced set of
+    orientations is returned by the function.
+    For more information on the method see:
+    https://doi.org/10.1107/S1600576719017138
+    '''
+    if texture == 'goss':
+        ang = [0, 45, 0]    # Euler angle for Goss texture
+        omega = 7.5         # kernel half-width
+    elif texture == 'copper':
+        ang = [90, 35, 45]
+        omega = 7.5
+    elif texture == 'random':
+        #For Random texture, no subsampling and ODF recreation is neccessary
+        ori_rve = knpy.createOrisetRandom(ms.Ngr)
+    else:
+        raise ValueError('texture not defined. Take goss, copper or random')
+    if texture != 'random':
+        ori_rve = knpy.createOriset(ms.Ngr, ang, omega, hist=mdf_freq['Low'],
+                                    shared_area=ms.shared_area)
     fname = ms.output_ang(ori=ori_rve, matname=matname, cut='xy', data='poly',
                           plot=False)
     # analyze result
     ebsd = knpy.EBSDmap(fname, matname)
     #ebsd.showIPF()
-    
+
     # write Euler angles of grains into Abaqus input file
     knpy.writeAbaqusMat(matnumber, ori_rve)
-    
+
     # write Abaqus input file for voxelated structure
     ms.output_abq('v')
