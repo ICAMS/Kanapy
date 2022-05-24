6ce25e15488fa60f44b4a5263d4b684b52e52149
AHartmaier <alexander.hartmaier@rub.de>
2022-05-24 16:49:45 +0200

    Adapted machine specific parts in test routine

--- src/kanapy/util.py
+++ src/kanapy/util.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))       # Kanapy's root
-MAIN_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),"../.."))
-HOME_DIR = os.path.expanduser('~')
-
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # Kanapy's root
 path = ''
 pel = ROOT_DIR.split('/')
 for hs in pel[0:pel.index('kanapy')+1]:
     path += hs+'/'
-    
-MAIN_DIR = path
-MTEX_DIR = path + 'libs/mtex'
+MAIN_DIR = path[0:-1]
+MTEX_DIR = path + '/libs/mtex'
+
+HOME_DIR = os.path.expanduser('~')  # User home directory
 KNPY_DIR = HOME_DIR + '/.kanapy'
 if not os.path.exists(KNPY_DIR):
     os.makedirs(KNPY_DIR)
