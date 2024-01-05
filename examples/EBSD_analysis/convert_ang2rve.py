7235e975e393dca92dd27983e05244369171972e
AHartmaier <alexander.hartmaier@rub.de>
2024-01-05 14:12:56 +0100

    Updated examples

--- examples/EBSD_analysis/convert_ang2rve.py
+++ examples/EBSD_analysis/convert_ang2rve.py
@@ -10,47 +10,36 @@ November 2023
 
 import kanapy as knpy
 import numpy as np
-    
+
+if not knpy.MTEX_AVAIL:
+    raise ModuleNotFoundError('MTEX module not available. Run kanapy setupTexture before using this script.')
+
 fname = 'ebsd_316L_500x500.ang'  # name of ang file to be imported
-matname = 'Iron fcc'  # material name
-matnumber = 4         # material number of austenite in CP UMAT
+matname = 'Iron_fcc'  # material name
 nvox = 30             # number of voxels per side
-box_length = 50       # side length of generated RVE
+box_length = 50       # side length of generated RVE in micron
 periodic = False      # create RVE with periodic structure
 
-
-if knpy.MTEX_AVAIL:
-    # read EBSD map and evaluate statistics of microstructural features
-    ebsd = knpy.EBSDmap(fname)
-    # ebsd.showIPF()
-    ms_data = ebsd.ms_data[0]
-    gs_param = ms_data['gs_param']
-    ar_param = ms_data['ar_param']
-    om_param = ms_data['om_param']
-    print('==== Grain size ====')
-    print(f'mean equivalent diameter: {gs_param[2].round(3)}, ' +
-          f'std. deviation: {gs_param[0].round(3)}, ' +
-          f'offset: {gs_param[1].round(3)}')
-    print('==== Aspect ratio ====')
-    print(f'mean value: {ar_param[2].round(3)}, ' +
-          f'std. deviation: {ar_param[0].round(3)}, ' +
-          f'offset: {ar_param[1].round(3)}')
-    print('==== Tilt angle ====')
-    print(f'mean value: {(om_param[1] * 180 / np.pi).round(3)}, ' +
-          f'std. deviation: {(om_param[0] * 180 / np.pi).round(3)}')
-else:
-    # MTEX is not available fall back to predefined values
-    print('*** Warning: Anaysis of EBSD maps is only possible with an ' +
-          'existing MTEX installation in Matlab.')
-    print('\nWill continue with predefined settings for microstructure.\n')
-    gs_param = np.array([1.06083584, 6.24824603, 13.9309554])
-    ar_param = np.array([0.67525027, 0.76994992, 1.69901906])
-    om_param = np.array([0.53941709, 1.44160447])
+# read EBSD map and evaluate statistics of microstructural features
+ebsd = knpy.EBSDmap(fname)
+# ebsd.showIPF()
+ms_data = ebsd.ms_data[0]  # analyse only data for majority phase with order parameter "0"
+gs_param = ms_data['gs_param']  # [std deviation, offset of lognorm distrib., mean grain size]
+ar_param = ms_data['ar_param']  # [std deviation, offset of gamma distrib., mean aspect ratio]
+om_param = ms_data['om_param']  # [std deviation, mean tilt angle]
+print('==== Grain size ====')
+print(f'mean equivalent diameter: {gs_param[2].round(3)}, ' +
+      f'std. deviation: {gs_param[0].round(3)}, ' +
+      f'offset: {gs_param[1].round(3)}')
+print('==== Aspect ratio ====')
+print(f'mean value: {ar_param[2].round(3)}, ' +
+      f'std. deviation: {ar_param[0].round(3)}, ' +
+      f'offset: {ar_param[1].round(3)}')
+print('==== Tilt angle ====')
+print(f'mean value: {(om_param[1] * 180 / np.pi).round(3)}, ' +
+      f'std. deviation: {(om_param[0] * 180 / np.pi).round(3)}')
 
 # create dictionary with statistical information obtained from EBSD map
-# gs_param : [std deviation, offset of lognorm distrib., mean grain size]
-# ar_param : [std deviation, offset of gamma distrib., mean aspect ration]
-# om_param : [std deviation, mean tilt angle]
 ms_stats = knpy.set_stats(gs_param, ar_param, om_param,
                           deq_min=8., deq_max=19., asp_min=0.95, asp_max=3.5,
                           omega_min=0., omega_max=2*np.pi, 
@@ -59,44 +48,21 @@ ms_stats = knpy.set_stats(gs_param, ar_param, om_param,
                           VF=1.0, phasename=matname, phasenum=0)
 
 # create and visualize synthetic RVE
+# create kanapy microstructure object
 ms = knpy.Microstructure(descriptor=ms_stats, name=fname+'_RVE')
-if knpy.MTEX_AVAIL:
-    ms.init_stats(gs_data=ms_data['gs_data'],
-                  ar_data=ms_data['ar_data'])
-else:
-    ms.init_stats()
-ms.init_RVE()
-ms.pack()
-ms.plot_ellipsoids()
-ms.voxelize()
-ms.plot_voxels(sliced=True)
-ms.generate_grains()
-ms.plot_grains()
-ms.res_data[0]['Unit_scale'] = 'um'
-ms.plot_stats(gs_param=gs_param, ar_param=ar_param)
+ms.init_RVE()  # initialize RVE including particle distribution and structured mesh
+# plot initial statistics of equivalent grain diameter and aspect ratio together with data from EBSD map
+ms.plot_stats_init(gs_data=ms_data['gs_data'], ar_data=ms_data['ar_data'])
+ms.pack()   # perform particle simulation to distribute grain nuclei in RVE volume
+ms.plot_ellipsoids()  # plot final configuration of particles
+ms.voxelize()  # assign voxels to grains according to particle configuration
+ms.plot_voxels(sliced=False)  # plot voxels colored according to grain number
+ms.generate_grains()  # generate a polyhedral hull around each voxelized grain
+ms.plot_grains()  # plot polyhedral grains
+ms.plot_stats(gs_param=gs_param, ar_param=ar_param)  # compare final grain statistics with initial parameters
 
-# compare microstructure on three surfaces 
-# for voxelated and polygonalized grains
-ms.plot_slice(cut='xz', pos='top', data='poly')
-ms.plot_slice(cut='yz', pos='top', data='poly')
-ms.plot_slice(cut='xy', pos='top', data='poly')
+# generate and assign grains orientations
+ms.generate_orientations(ebsd)
 
-if not knpy.MTEX_AVAIL:
-    raise ModuleNotFoundError('Generation of grain orientation sets is only ' +
-         'possible with an existing MTEX installation in Matlab.')
-# get list of orientations for grains in RVE matching the ODF of the EBSD map
-ori_rve = ebsd.calcORI(ms.Ngr, shared_area=ms.shared_area)
-
-# export virtual EBSD map as ang file and analyse with MTEX
-ang_rve = ms.output_ang(ori=ori_rve, cut='xy', pos='top', cs=ms_data['cs'],
-                        matname=matname, plot=False)
-ebsd_rve = knpy.EBSDmap(ang_rve, matname)
-
-# write Abaqus input file for voxelated structure
-ms.output_abq('v')
-# write Euler angles of grains into Abaqus input file
-knpy.writeAbaqusMat(matnumber, ori_rve)
-
-# smoothen voxelated structure and write Abaqus .inp file
-ms.smoothen()
-ms.output_abq('s')
+# output rve in voxel format
+ms.write_voxels(script_name=__file__, mesh=False, system=False)
--- examples/RVE_generation/generate_GBtexture.py
+++ examples/RVE_generation/generate_GBtexture.py
@@ -1,101 +1,98 @@
-00#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Analyse grain geometries of polycrystals, extract statistical information
-form 2D slices, comaparable to experiment, fit ellipsoids to 3D grains
-(polyhedra) and exctract statistical information
+Create RVE based on statistical microstructure information
 
 @author: Alexander Hartmaier, ICAMS / Ruhr-University Bochum, Germany
-August 2022
+January 2024
 
 """
 
-import numpy as np
 import kanapy as knpy
+from math import pi
 
-texture = 'random'  # Implemented textures are goss, copper, random
-matname = 'Simulanium fcc'
-matnumber = 4  # UMAT number for fcc Iron
-Nv = 30
-size = 40
-periodic = False
+if not knpy.MTEX_AVAIL:
+    raise ModuleNotFoundError("The module MTEX is required for this example, please run 'kanapy setupTexture' first.")
+
+texture = 'goss'  # chose texture type 'random', 'goss' or 'copper'
+matname = 'Simulanium'  # Material name
+nvox = 30  # number of voxels in each Cartesian direction
+size = 40  # size of RVE in micron
+periodic = False  # create periodic (True) or non-periodic (False) RVE
+# define statistical information on microstructure
 ms_elong = {'Grain type': 'Elongated',
-          'Equivalent diameter': {'std': 1.0, 'mean': 12.0, 'offs': 4.0, 'cutoff_min': 8.0, 'cutoff_max': 18.0},
-          'Aspect ratio': {'std':1.5, 'mean': 2.0, 'offs': 0.8, 'cutoff_min': 1.0, 'cutoff_max': 4.0},
-          'Tilt angle': {'std': 15., 'mean': 90., "cutoff_min": 0.0, "cutoff_max": 180.0},
-          'RVE': {'sideX': size, 'sideY': size, 'sideZ': size, 'Nx': Nv, 'Ny': Nv, 'Nz': Nv},
-          'Simulation': {'periodicity': str(periodic), 'output_units': 'mm'}}
+            'Equivalent diameter': {'std': 1.0, 'mean': 12.0, 'offs': 4.0, 'cutoff_min': 8.0, 'cutoff_max': 18.0},
+            'Aspect ratio': {'std': 0.5, 'mean': 2.0, 'offs': 0.8, 'cutoff_min': 1.0, 'cutoff_max': 3.0},
+            "Tilt angle": {"std": 1.0, "mean": 0.5*pi, "cutoff_min": 0.0, "cutoff_max": 2*pi},
+            'RVE': {'sideX': size, 'sideY': size, 'sideZ': size, 'Nx': nvox, 'Ny': nvox, 'Nz': nvox},
+            'Simulation': {'periodicity': str(periodic), 'output_units': 'um'}}
 
-ms = knpy.Microstructure(ms_elong)
-ms.init_stats()
-ms.init_RVE()
-ms.pack()
-ms.plot_ellipsoids()
-ms.voxelize()
-ms.plot_voxels()
-ms.generate_grains()
-ms.plot_grains()
-
-# compare microstructure on three surfaces
-# for voxelated and polygonalized grains
-ms.plot_slice(cut='yz', pos='top', data='poly')
-ms.plot_slice(cut='xz', pos='top', data='poly')
-ms.plot_slice(cut='xy', pos='top', data='poly')
+# create and visualize synthetic RVE
+# create kanapy microstructure object
+ms = knpy.Microstructure(descriptor=ms_elong, name=matname + '_' + texture + '_texture')
+ms.init_RVE()  # initialize RVE including particle distribution and structured mesh
+ms.plot_stats_init()  # plot initial statistics of equivalent grain diameter and aspect ratio
+ms.pack()  # perform particle simulation to distribute grain nuclei in RVE volume
+ms.plot_ellipsoids()  # plot final configuration of particles
+ms.voxelize()  # assign voxels to grains according to particle configuration
+ms.plot_voxels(sliced=False)  # plot voxels colored according to grain number
+ms.generate_grains()  # generate a polyhedral hull around each voxelized grain
+ms.plot_grains()  # plot polyhedral grains
+ms.plot_stats()  # compare final grain statistics with initial parameters
 
 # Test cases for misorientation distribution (MDF)
-'''adapted from
+"""adapted from
 Miodownik, M., et al. "On boundary misorientation distribution functions
 and how to incorporate them into three-dimensional models of microstructural
 evolution." Acta materialia 47.9 (1999): 2661-2668.
 https://doi.org/10.1016/S1359-6454(99)00137-8
-'''
-if knpy.MTEX_AVAIL:
-    mdf_freq = {
-      "High": [0.0013303016, 0.208758929, 0.3783092708, 0.7575794912, 1.6903069613,
-               2.5798481069, 5.0380640643, 10.4289690569, 21.892113657, 21.0,
-               22.1246762077, 13.9000439533],
-      "Low":  [4.5317, 18.6383, 25, 20.755, 12.5984, 7.2646, 4.2648, 3.0372, 2.5,
-               1, 0.31, 0.1],
-      "Random": [0.1, 0.67, 1.9, 3.65, 5.8, 8.8, 11.5, 15.5, 20, 16.7, 11.18, 4.2]
-    }
-    mdf_bins = np.linspace(62.8/12,62.8,12)
+"""
+mdf_freq = {
+    "High": [0.0013303016, 0.208758929, 0.3783092708, 0.7575794912, 1.6903069613,
+             2.5798481069, 5.0380640643, 10.4289690569, 21.892113657, 21.0,
+             22.1246762077, 13.9000439533],
+    "Low": [4.5317, 18.6383, 25, 20.755, 12.5984, 7.2646, 4.2648, 3.0372, 2.5,
+            1, 0.31, 0.1],
+    "Random": [0.1, 0.67, 1.9, 3.65, 5.8, 8.8, 11.5, 15.5, 20, 16.7, 11.18, 4.2]
+}
+# mdf_bins = np.linspace(62.8 / 12, 62.8, 12)
 
-    # generate grain orientations and write ang file
-    '''
-    Different textures can be choosen and assinged to the RVE geometry that has
-    been defined above.
-    Texture is defined by the orientation of the ideal component in Euler space
-    ang and a kernel halfwidth omega. Kernel used here is deLaValleePoussin.
-    The function createOriset will first create an artificial EBSD by sampling
-    a large number of discrete orientations from the ODF defined by ang and
-    omega. Then a reduction method is applied to reconstruct this initial ODF
-    with a smaller number of discrete orientations Ngr. The reduced set of
-    orientations is returned by the function.
-    For more information on the method see:
-    https://doi.org/10.1107/S1600576719017138
-    '''
-    if texture == 'goss':
-        ang = [0, 45, 0]    # Euler angle for Goss texture
-        omega = 7.5         # kernel half-width
-    elif texture == 'copper':
-        ang = [90, 35, 45]
-        omega = 7.5
-    elif texture == 'random':
-        #For Random texture, no subsampling and ODF recreation is neccessary
-        ori_rve = knpy.createOrisetRandom(ms.Ngr)
-    else:
-        raise ValueError('texture not defined. Take goss, copper or random')
-    if texture != 'random':
-        ori_rve = knpy.createOriset(ms.Ngr, ang, omega, hist=mdf_freq['Low'],
-                                    shared_area=ms.shared_area)
-    fname = ms.output_ang(ori=ori_rve, matname=matname, cut='xy', data='poly',
-                          plot=False)
-    # analyze result
-    ebsd = knpy.EBSDmap(fname, matname)
-    #ebsd.showIPF()
+# generate grain orientations
+"""
+Different textures can be chosen and assigned to the RVE geometry that has
+been defined above.
+Texture is defined by the orientation of the ideal component in Euler space
+ang and a kernel half-width omega. Kernel used here is deLaValleePoussin.
+The function createOriset will first create an artificial EBSD by sampling
+a large number of discrete orientations from the ODF defined by ang and
+omega. Then a reduction method is applied to reconstruct this initial ODF
+with a smaller number of discrete orientations Ngr. The reduced set of
+orientations is returned by the function.
+For more information on the method see:
+https://doi.org/10.1107/S1600576719017138
+"""
+if texture == 'goss':
+    desc = 'unimodal'
+    ang = [0, 45, 0]  # Euler angles (in degrees) for unimodal Goss texture
+    omega = 7.5  # kernel half-width in degree
+    Nbase = 5000  # number of orientations from which set is sub-sampled
+elif texture == 'copper':
+    desc = 'unimodal'
+    ang = [90, 35, 45]  # Euler angles (in degrees) for unimodal copper texture
+    omega = 7.5
+    Nbase = 5000
+elif texture == 'random':
+    desc = 'random'
+    ang = None  # for Random texture, no ODF recreation is necessary
+    omega = None
+    Nbase = 5000
+else:
+    raise ValueError('Texture not defined. Use "goss", "copper" or "random"')
 
-    # write Euler angles of grains into Abaqus input file
-    knpy.writeAbaqusMat(matnumber, ori_rve)
+# create grain orientations for Goss or random texture
+ms.generate_orientations(desc, ang=ang, omega=omega, Nbase=Nbase,
+                         hist=mdf_freq['Low'],
+                         shared_area=ms.geometry['GBarea'])
 
-    # write Abaqus input file for voxelated structure
-    ms.output_abq('v')
+# output rve in voxel format
+ms.write_voxels(script_name=__file__, mesh=False, system=False)
