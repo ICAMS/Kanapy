b1c8bb4de9d067cac700c3fd778900b5c2606696
AHartmaier <alexander.hartmaier@rub.de>
2024-03-03 12:13:23 +0100

    Version 6.3.1 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- docs/builds/html/_static/documentation_options.js
+++ docs/builds/html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '6.1.6',
+    VERSION: '6.3.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open(path_path, 'w') as outfile:
 # execute setup procedure
 setup(
     name='kanapy',
-    version='6.3.0',
+    version='6.3.1',
     author='Mahesh R.G. Prasad, Abhishek Biswas, Golsa Tolooei Eshlaghi, Napat Vajragupta, Alexander Hartmaier',
     author_email='alexander.hartmaier@rub.de',
     classifiers=[        
