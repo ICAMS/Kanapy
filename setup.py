5a9e875b494a661e6889d4c87f4d5d7ef7d2f53c
AHartmaier <alexander.hartmaier@rub.de>
2025-06-09 15:10:28 +0200

    Version 6.4.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open(path_path, 'w') as outfile:
 # execute setup procedure
 setup(
     name='kanapy',
-    version='6.4.0',
+    version='6.4.1',
     author='Mahesh R.G. Prasad, Abhishek Biswas, Golsa Tolooei Eshlaghi, Ronak Shoghi, '
            'Napat Vajragupta, Alexander Hartmaier',
     author_email='alexander.hartmaier@rub.de',
