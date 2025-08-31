ae4989478387396627fbf0c54cda3978df057f20
AHartmaier <alexander.hartmaier@rub.de>
2025-08-31 10:44:37 +0200

    Improvements in texture plotting

--- src/kanapy/__init__.py
+++ src/kanapy/__init__.py
@@ -26,7 +26,7 @@ from .core import Microstructure, set_stats, pickle2microstructure, import_voxel
      import_stats, write_stats, start, triple_surf, plot_voxels_3D, plot_polygons_3D
 
 from .texture import EBSDmap, ODF, createOriset, createOrisetRandom, \
-    get_ipf_colors, plot_inverse_pole_figure, plot_inverse_pole_figure_density
+    get_ipf_colors, plot_pole_figure, plot_pole_figure_proj
 
 log_level = 20  # Levels for logging: 10: DEBUG, 20: INFO, 30: WARNING, 40: ERROR
 logging.basicConfig(level=log_level)  # set log level
@@ -38,6 +38,6 @@ __version__ = version('kanapy')
 __backend__ = "orix"
 __all__ = ["Microstructure", "set_stats", "pickle2microstructure", "import_voxels",
            "import_stats", "write_stats", "start", "EBSDmap", "ODF",  "createOriset",
-           "createOrisetRandom", "get_ipf_colors", "plot_inverse_pole_figure",
-           "plot_inverse_pole_figure_density", "plot_voxels_3D", "plot_polygons_3D"]
+           "createOrisetRandom", "get_ipf_colors", "plot_pole_figure",
+           "plot_pole_figure_proj", "plot_voxels_3D", "plot_polygons_3D"]
 MTEX_AVAIL = __backend__  # legacy flag for downwards compatibility
